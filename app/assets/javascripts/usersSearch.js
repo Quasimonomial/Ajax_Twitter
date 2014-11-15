9c713f87f7825dc427baba53926aeb696aaf0de9
Travis Herrick <Quasimonomial@gmail.com>
2014-11-14 16:40:27 -0800

    Add input event handler to user search box

--- app/assets/javascripts/usersSearch.js
+++ app/assets/javascripts/usersSearch.js
@@ -2,8 +2,53 @@
 
 $.UsersSearch = function (el) {
   this.$el = $(el);
-  console.log("YEAY")
+  this.$input = $(this.$el.find("input"));
+  this.$ul = $(this.$el.find("ul"));
+  this.bindListener();
+  
 };
+
+$.UsersSearch.prototype.bindListener = function (){
+  this.$input.on("input", this.handleInput.bind(this));
+};
+
+$.UsersSearch.prototype.handleInput = function(event){
+  var that = this;
+  var formData = { query: this.$input.val() };
+  $.ajax("/users/search", {
+    type: 'GET',
+    datatype: "json",
+    data: formData,
+    success: function( resp ) {
+      console.log("successful query");
+      that.renderResults(resp);
+    },
+    error: function() {
+      console.log("You're both fucking failures and will never amount to anything.")
+    }
+  })
+}
+
+$.UsersSearch.prototype.renderResults = function(resp) {
+  console.log(resp)
+}
+
+
+
+// type: 'POST',
+// data: requestData,
+// dataType: 'json',
+// success: function( resp ) {
+//   that.$el.attr('data-initial-follow-state', "followed");
+//   that.followState = "followed";
+//   console.log("New Follow State: " + that.followState);
+//   that.render();
+// },
+// error: function() {
+//   console.log("fail at following :'(");
+// }
+
+
 
 $.fn.usersSearch = function () {
   return this.each(function () {
